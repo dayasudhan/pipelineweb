0a3ca896b8453f5a8a4689e9e02475cb49223fe1
Dayasudhan <dayasudhankg@gmail.com>
2019-10-12 22:59:20 +0530

    pline geojson

--- app/routes.js
+++ app/routes.js
@@ -1185,25 +1185,19 @@ app.get( '/v1/plinemap/nearby', function( request, response ) {
   //         }
   //       }
   //     }
-  //   }
-  // coordinates: [[
-  //   [-102, 41],
-  //   [-102, 37],
-  //   [-109, 37],
-  //   [-109, 41]
-  // ]]
- // var geojsonPoly2 = { type: 'Polygon', coordinates: [[[-5,-5], ['-5',5], [5,5], [5,-5],[-5,'-5']]] }
-  var geojsonPoly = { type: 'Polygon', coordinates: [[
-    [14.1603438,75.6205914],
-    [14.0697727,75.6018832],
-    [14.0510405,75.7768592],
-    [14.2538865,75.7388695],
-    [14.1603438,75.6205914]
-  ]] }
+  //   }\
+  console.log(request.headers.geojsonPoly);
+  // var geojsonPoly = { type: 'Polygon', coordinates: [[
+  //   [14.1603438,75.6205914],
+  //   [14.0697727,75.6018832],
+  //   [14.0510405,75.7768592],
+  //   [14.2538865,75.7388695],
+  //   [14.1603438,75.6205914]
+  // ]] }
 // Model.find({ loc: { $within: { $geometry: geojsonPoly }}})
 // // or
 // Model.where('loc').within.geometry(geojsonPoly)
-  return PlineModel.find({'location.coordinates':{ $within: { $geometry: geojsonPoly }}},function( err, order ) {
+  return PlineModel.find({'location.coordinates':{ $within: { $geometry: request.headers.geojsonPoly }}},function( err, order ) {
       if( !err ) {
           return response.send( order );
       } else {
