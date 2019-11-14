5c237e3b1fa8ac861c39e0ab474da48d21610d98
Dayasudhan <dayasudhankg@gmail.com>
2019-11-14 17:45:09 +0530

    pipe_type

--- app/routes.js
+++ app/routes.js
@@ -475,7 +475,7 @@ app.post( '/v1/pline/:id', function( request, response ) {
       date:indiantime,
       size:request.body.size,
       remarks:request.body.remarks,
-      pipe_type:request.body.type,
+      pipe_type:request.body.pipe_type,
       purpose:request.body.purpose,
       location: request.body }; 
       var pipeline = new PlineModel(pline);
